d707e798c6fcaa45a42479d34faddd3a32952286
drewcgibbons <drewcgibbons@gmail.com>
2025-02-09 23:12:54 -0600

    Homepage modification

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import PageNavbar from '../../components/PageNavbar/PageNavbar';
 import Footer from '../../components/Footer/Footer';
 import "./Home.css"
+import Card from '../../components/Card/Card';
+
 
 const Home : React.FC = () => {
     return(
         <>
         <PageNavbar/>
+        <Card></Card>
         <Footer/>
         </>
     );
